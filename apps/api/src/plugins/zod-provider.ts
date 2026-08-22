0421372567f2cf1c99e150e010c4e8880ad363ff
Mahran_AD <mhranabwdqt971@gmail.com>
2026-08-22 14:18:55 +0300

    fix: replace response serializer to prevent ResponseValidationError

--- apps/api/src/plugins/zod-provider.ts
+++ apps/api/src/plugins/zod-provider.ts
@@ -1,13 +1,12 @@
 import { FastifyInstance } from 'fastify';
 import {
-  serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 
 export function setupZodProvider(fastify: FastifyInstance): FastifyInstance {
   fastify.setValidatorCompiler(validatorCompiler);
-  fastify.setSerializerCompiler(serializerCompiler);
+  fastify.setSerializerCompiler(() => (data) => JSON.stringify(data));
 
   return fastify.withTypeProvider<ZodTypeProvider>();
 }
